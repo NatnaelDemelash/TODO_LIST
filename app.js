dd4b032bdf12133539af434a195a2692e0b8a500
Natnael <natidemelash074@gmail.com>
2022-12-09 15:52:05 +0300

    initial setup

---
+++ app.js
@@ -0,0 +1,14 @@
+//SELECTOR
+const todoInput = document.querySelector('.todo-input');
+const todoBtn = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//#EVENT LISTNER
+
+todoBtn.addEventListener('click', addTodo);
+
+//FUNCTION
+
+function addTodo() {
+
+}
